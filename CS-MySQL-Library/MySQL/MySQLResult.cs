417b1370d1d20be6b5e91e1f4e13659365202a23
GHMatti <33543464+GHMatti@users.noreply.github.com>
2018-03-21 19:26:09 +0100

    Initial Commit

---
+++ CS-MySQL-Library/MySQL/MySQLResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace GHMatti.MySQL
+{
+    /// <summary>
+    /// Renaming the Result from Queries to something shorter
+    /// </summary>
+    public class MySQLResult : List<Dictionary<string, dynamic>> { }
+}
